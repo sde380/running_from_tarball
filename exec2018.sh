c24fa07876916b7e86ff407629a64be3d8105633
Quantumapple <jongapples@gmail.com>
2020-10-07 00:51:53 +0900

    typo fix

--- exec2018.sh
+++ exec2018.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#export X509_USER_PROXY=${PWD}/x509up
 export HOME=${PWD}
 
 tar xvaf submit.tgz
 cd submit
-. runEventGeneration2018.sh
+bash ./runEventGeneration2018_slc7.sh
 cd ${HOME}
 rm -r submit/
 exit 0
