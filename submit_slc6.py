186678355555ca0b07ad68a62ba56c7cf639c73f
Jongho Lee <jongho.lee@cern.ch>
2022-11-05 17:46:34 -0500

    update singularity image

--- submit_slc6.py
+++ submit_slc6.py
@@ -20,7 +20,7 @@ output = {1}/$(Cluster)_$(Process).out
 error = {1}/$(Cluster)_$(Process).err
 log = {1}/$(Cluster)_$(Process).log
 arguments = $(Process)
-+SingularityImage = "/cvmfs/singularity.opensciencegrid.org/bbockelm/cms:rhel6"
++SingularityImage = "/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel6"
 request_memory = 4096
 queue {3}
 '''.format(workpath,logpath,uid,njobs)
@@ -29,3 +29,4 @@ with open(logpath+'/condor.jdl','w') as jdlfile:
   jdlfile.write(classad)
 
 system('condor_submit %s/condor.jdl'%logpath)
+#+SingularityImage = "/cvmfs/singularity.opensciencegrid.org/bbockelm/cms:rhel6"
