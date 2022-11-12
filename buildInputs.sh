fc3d056666a109f317e533813a717b1195099cbc
Jongho Lee <jongho.lee@cern.ch>
2022-11-11 22:00:51 -0600

    update

--- buildInputs.sh
+++ buildInputs.sh
@@ -2,10 +2,10 @@
 BASEDIR=${PWD}
 
 ##### Change mass point ####
-mZ=1000
-mHS=110
-mCHI=500
 year=${2}
+mZ=${3}
+mHS=${4}
+mCHI=${5}
 
 mkdir -p ./submit/input/
 cp ./inputs/nanotools.tar ./submit/input/
@@ -15,6 +15,7 @@ data=${1}
 if [ ${data} = "MonoHs" ]; then
 
     dname=DarkHiggs_MonoHs_LO_${year}
+    gridpack=DarkHiggs_MonoHs_LO_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_gSM-0p25_gDM-1p0_th-0p01_13TeV-madgraph_slc7_amd64_gcc630_CMSSW_9_3_8_tarball.tar.xz 
 
     echo "You're producing mono dark Higgs sample!"
     cp ${BASEDIR}/hadronizer/${dname}_hadronizer.py ${BASEDIR}/hadronizer/${dname}_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_hadronizer.py
@@ -22,7 +23,7 @@ if [ ${data} = "MonoHs" ]; then
     sed -i "s/mz/${mZ}/g" ${BASEDIR}/hadronizer/${dname}_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_hadronizer.py
     sed -i "s/mhs/${mHS}/g" ${BASEDIR}/hadronizer/${dname}_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_hadronizer.py
     sed -i "s/mchi/${mCHI}/g" ${BASEDIR}/hadronizer/${dname}_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_hadronizer.py
-
+    
     ##### Check parameters are properly changed #####
     cat ${BASEDIR}/hadronizer/${dname}_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_hadronizer.py | grep "gridpack ="
     echo ""
@@ -40,6 +41,7 @@ if [ ${data} = "MonoHs" ]; then
     
     if [ ${year} -eq 2016 ]; then
         mv ${BASEDIR}/hadronizer/${dname}_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_hadronizer.py ./submit/input/
+        cp gridpacks/gridpack2016/${gridpack} ./submit/input
         cp inputs/aod_template2016.py ./submit/input/
         cp inputs/pu_files2016.py ./submit/input/
         cp inputs/mc_NANO_2016.py ./submit/input/
@@ -47,7 +49,8 @@ if [ ${data} = "MonoHs" ]; then
     fi
 
     if [ ${year} -eq 2017 ]; then
-        mv ${BASEDIR}/inputs/${dname}_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_hadronizer.py ./submit/input/
+        mv ${BASEDIR}/hadronizer/${dname}_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_hadronizer.py ./submit/input/
+        cp gridpacks/gridpack2017_2018/${gridpack} ./submit/input
         cp inputs/aod_template2017.py ./submit/input/
         cp inputs/pu_files2017.py ./submit/input/
         cp inputs/mc_NANO_2017.py ./submit/input/
@@ -55,7 +58,8 @@ if [ ${data} = "MonoHs" ]; then
     fi
 
     if [ ${year} -eq 2018 ]; then
-        mv ${BASEDIR}/inputs/${dname}_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_hadronizer.py ./submit/input/
+        mv ${BASEDIR}/hadronizer/${dname}_MZprime-${mZ}_Mhs-${mHS}_Mchi-${mCHI}_hadronizer.py ./submit/input/
+        cp gridpacks/gridpack2017_2018/${gridpack} ./submit/input
         cp inputs/aod_template2018.py ./submit/input/
         cp inputs/pu_files2018.py ./submit/input/
         cp inputs/mc_NANO_2018.py ./submit/input/
