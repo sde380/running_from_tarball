e49319afd1a69403619f595d2794f7027962c1fd
Jongho Lee <jongho.lee@cern.ch>
2023-10-18 23:42:28 -0500

    monoHiggs hadronizers

---
+++ hadronizer/monoHiggsToBB_2hdma/hadronizer_2HDMa_bb_sinp_0p35_tanb_0p8_mXd_10_MH3_600_MH4_200_MH2_600_MHC_600.py
@@ -0,0 +1,38 @@
+
+import FWCore.ParameterSet.Config as cms
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc630/13TeV/madgraph/V5_2.6.0/monoHiggs/2HDMa/2HDMa_bb_sinp_0p35_tanb_0p8_mXd_10_MH3_600_MH4_200_MH2_600_MHC_600_slc6_amd64_gcc630_CMSSW_9_3_8_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(500),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                        maxEventsToPrint = cms.untracked.int32(1),
+                        pythiaPylistVerbosity = cms.untracked.int32(1),
+                        filterEfficiency = cms.untracked.double(1.0),
+                        pythiaHepMCVerbosity = cms.untracked.bool(False),
+                        comEnergy = cms.double(13000.),
+                        PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'SLHA:useDecayTable = off',  # Use pythia8s own decay mode instead of decays defined in LH accord
+            '25:m0 = 125.0',
+            '25:onMode = off',
+            '25:onIfMatch = 5 -5'
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters'
+                                    )
+        )
+                        )
+
+ProductionFilterSequence = cms.Sequence(generator)
